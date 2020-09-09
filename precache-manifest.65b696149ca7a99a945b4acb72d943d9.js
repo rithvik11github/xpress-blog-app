c388e27e012c1aa0c6cc05559643d73026a52eff
Rithvik <rithvikseth11@gmail.com>
2020-09-10 00:29:31 +0530

    Updates

---
+++ precache-manifest.65b696149ca7a99a945b4acb72d943d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d8074e82fc5d23f1da33ba97fccd2",
+    "url": "/xpress-blog-app/index.html"
+  },
+  {
+    "revision": "0c0d2348aab7ee0f3c94",
+    "url": "/xpress-blog-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fff8a150a90f73710b1a",
+    "url": "/xpress-blog-app/static/css/main.f64343a9.chunk.css"
+  },
+  {
+    "revision": "0c0d2348aab7ee0f3c94",
+    "url": "/xpress-blog-app/static/js/2.c1643bfe.chunk.js"
+  },
+  {
+    "revision": "b8fa2dc5aa7f3d680fd19a256eb70b35",
+    "url": "/xpress-blog-app/static/js/2.c1643bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff8a150a90f73710b1a",
+    "url": "/xpress-blog-app/static/js/main.3084615e.chunk.js"
+  },
+  {
+    "revision": "39e53169d83d0cd9903b",
+    "url": "/xpress-blog-app/static/js/runtime-main.72e4a096.js"
+  },
+  {
+    "revision": "7fd4639b66c0a78ecc411c5cccbae1cf",
+    "url": "/xpress-blog-app/static/media/finance.7fd4639b.png"
+  },
+  {
+    "revision": "d6b1bf13a479b4e12a602bd9595e503d",
+    "url": "/xpress-blog-app/static/media/fitness.d6b1bf13.jpg"
+  },
+  {
+    "revision": "5e29ba0efaa4f62870749fefa865749e",
+    "url": "/xpress-blog-app/static/media/food.5e29ba0e.jpg"
+  },
+  {
+    "revision": "9059802c464a0e44b04349f9e7fc3277",
+    "url": "/xpress-blog-app/static/media/politics.9059802c.jpg"
+  },
+  {
+    "revision": "050927b5dff61f73db4e19f63d5165c4",
+    "url": "/xpress-blog-app/static/media/restaurant.050927b5.jpg"
+  },
+  {
+    "revision": "8af478e56af8b3986faece45a5119480",
+    "url": "/xpress-blog-app/static/media/travel.8af478e5.png"
+  }
+]);
